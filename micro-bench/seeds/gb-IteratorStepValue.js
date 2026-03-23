f82084d303cbfa6288c36ad5dd17793eee23a0ed
d01c2 <rickykhj@korea.ac.kr>
2026-03-23 14:24:18 +0900

    feat: added hard-to-cover branches in micro-bench

---
+++ micro-bench/seeds/gb-IteratorStepValue.js
@@ -0,0 +1,5 @@
+// Branch[2417] GroupBy
+// target: IteratorStepValue(iteratorRecord) (= abrupt)
+// taken: false side taken (not abrupt), so have to target true side
+"use strict";
+Map.groupBy("", x => x);
---
+++ micro-bench/seeds/ts-toString.js
@@ -0,0 +1,5 @@
+// Branch[1430] ToString
+// target: ToString(primValue) (= abrupt)
+// taken: false side taken (not abrupt), so have to target true side
+"use strict";
++ [ [ ] ];
