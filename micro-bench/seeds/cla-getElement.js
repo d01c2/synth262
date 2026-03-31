abbafe454d29fa6ac0beb6aab3af00bffb90b1c5
d01c2 <rickykhj@korea.ac.kr>
2026-03-31 17:26:21 +0900

    bench: add CreateListFromArrayLike micro-bench seed

---
+++ micro-bench/seeds/cla-getElement.js
@@ -0,0 +1,5 @@
+// Branch[2049] CreateListFromArrayLike
+// target: Get(obj, indexName) (= abrupt)
+// taken: false side taken (not abrupt), so have to target true side
+"use strict";
+Function.apply(1, [,]);
